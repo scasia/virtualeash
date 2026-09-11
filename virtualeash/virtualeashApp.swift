99acb9664c1e0f3865c75ae4fb1f820f8efc9066
Kasia <121980839+scasia@users.noreply.github.com>
2026-09-11 21:38:42 +0200

    Initial Commit

---
+++ virtualeash/virtualeashApp.swift
@@ -0,0 +1,17 @@
+//
+//  virtualeashApp.swift
+//  virtualeash
+//
+//  Created by kasia on 11/09/2026.
+//
+
+import SwiftUI
+
+@main
+struct virtualeashApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
